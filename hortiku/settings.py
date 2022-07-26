7065f719ae699afe7e22b7110d77aea11f9245ea
Muhammad Ishak <muhammadishak003@gmail.com>
2022-07-26 09:08:51 +0800

    update terbaru

--- hortiku/settings.py
+++ hortiku/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pangan',
 ]
 
 MIDDLEWARE = [
@@ -116,10 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL='/media/'
-STATICFILES_DIRS=[BASE_DIR]
-STATIC_ROOT='/home/u1717982/hortiku/static'
-MEDIA_ROOT='/home/u1717982/hortiku/media'
+STATICFILES_DIRS = [
+    BASE_DIR,
+    "static",
+]
+# khusus untuk deploy nanti
+# MEDIA_URL='/media/'
+# STATICFILES_DIRS=[BASE_DIR]
+# STATIC_ROOT='/home/u1717982/hortiku/static'
+# MEDIA_ROOT='/home/u1717982/hortiku/media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- hortiku/views.py
+++ hortiku/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.conf import settings
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'heading': 'HORTIKU.COM',
+        'subheading': 'Petani sejahtera, Negara Maju',
+        'banner': 'img/logo hortiku.png',
+    }
+    return render(request, 'index.html', context)
